faa222871eba097a045afdfed366743550f7abc5
Wandering Crow <FraternaliAlessandro@gmail.com>
2020-04-17 21:31:03 +0200

    Inseriti commenti guida

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  // ogni click sui div invia una richiesta AJAX che prende un numero random da 1 a 9 (primo end-point della API in slide).
+
+  // Se il num ritornato è <= 5 il div diventa giallo
+
+  // se è > di 5 il div diventa verde
+
+  // Il numero ottenuto appare al centro del quadrato
+});
