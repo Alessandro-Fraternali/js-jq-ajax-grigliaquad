c4cbde85a98f11801efe075b538fc099478df132
Wandering Crow <FraternaliAlessandro@gmail.com>
2020-04-18 17:14:47 +0200

    Implementato un alert che avvisa se provi a ricliccare un elemento

--- js/main.js
+++ js/main.js
@@ -2,32 +2,31 @@ $(document).ready(function() {
   // ogni click sui div invia una richiesta AJAX che prende un numero random da 1 a 9
   $('.quadratini').on('click', function(){
       var theChosenOne = $(this);
-      $.ajax({
-        url : "https://flynn.boolean.careers/exercises/api/random/int",
-        method : "GET",
-        success : function (data,stato) {
-          // Se il num ritornato è <6 il div diventa giallo
-          if (data.response < 6){
-            theChosenOne.addClass("giallo cliccato");
-            theChosenOne.append('<h1>' + data.response + '</h1>');
-            // Il numero ottenuto appare al centro del quadrato
+      if (theChosenOne.hasClass("cliccato")){
+        alert("Hai già ricevuto questo risultato!");
+      }else{
+        $.ajax({
+          url : "https://flynn.boolean.careers/exercises/api/random/int",
+          method : "GET",
+          success : function (data,stato) {
+            // Se il num ritornato è <6 il div diventa giallo
+            if (data.response < 6){
+              theChosenOne.addClass("giallo cliccato");
+              theChosenOne.append('<h1>' + data.response + '</h1>');
+              // Il numero ottenuto appare al centro del quadrato
+            }
+            // se è > di 5 il div diventa verde
+            else {
+              theChosenOne.addClass("verde cliccato");
+              theChosenOne.append('<h1>' + data.response + '</h1>');
+              // Il numero ottenuto appare al centro del quadrato
+            }
+          },
+          error : function (richiesta,stato,errori) {
+            console.log("nope");
           }
-          // se è > di 5 il div diventa verde
-          else {
-            theChosenOne.addClass("verde cliccato");
-            theChosenOne.append('<h1>' + data.response + '</h1>');
-            // Il numero ottenuto appare al centro del quadrato
-          }
-        },
-        error : function (richiesta,stato,errori) {
-          console.log("nope");
-        }
-      });
+        });
+      }
     }
   )
-
-  $('.quadratone').on("click", ".cliccato",
-     function () {
-       alert("Hai già cliccato questo quadrato");
-  });
 });
