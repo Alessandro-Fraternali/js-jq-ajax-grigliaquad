85fe1ab267ff6b8be2b2a82f7fceaac929141068
Wandering Crow <FraternaliAlessandro@gmail.com>
2020-04-18 15:55:01 +0200

    Chiamata Ajax e cambio colore dei div in base a data.response completato

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
 $(document).ready(function() {
-  // ogni click sui div invia una richiesta AJAX che prende un numero random da 1 a 9 (primo end-point della API in slide).
+  // ogni click sui div invia una richiesta AJAX che prende un numero random da 1 a 9
+  $('.quadratini').on('click', function(){
+      var theChosenOne = $(this);
+      $.ajax({
+        url : "https://flynn.boolean.careers/exercises/api/random/int",
+        method : "GET",
+        success : function (data,stato) {
+          // Se il num ritornato è <6 il div diventa giallo
+          if (data.response < 6){
+            theChosenOne.addClass("giallo cliccato");
+            theChosenOne.append('<h1>' + data.response + '</h1>');
+            // Il numero ottenuto appare al centro del quadrato
+          }
+          // se è > di 5 il div diventa verde
+          else {
+            theChosenOne.addClass("verde cliccato");
+            theChosenOne.append('<h1>' + data.response + '</h1>');
+            // Il numero ottenuto appare al centro del quadrato
+          }
+        },
+        error : function (richiesta,stato,errori) {
+          console.log("nope");
+        }
+      });
+    }
+  )
 
-  // Se il num ritornato è <= 5 il div diventa giallo
-
-  // se è > di 5 il div diventa verde
-
-  // Il numero ottenuto appare al centro del quadrato
+  $('.quadratone').on("click", ".cliccato",
+     function () {
+       alert("Hai già cliccato questo quadrato");
+  });
 });
